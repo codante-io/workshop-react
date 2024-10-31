1b1ab41e996995976129da1f987d25a7c1cd72ca
Ícaro Harry <me@icaro.io>
2024-10-31 19:48:56 -0300

    Final challenge

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { PropertiesContext } from "../providers/property-provider";
+
 function Hero() {
+  const { searchText, setSearchText } = useContext(PropertiesContext);
+
   return (
     <section className="bg-gray-100 py-20 text-gray-700">
       <div className="container mx-auto px-12">
@@ -10,9 +15,13 @@ function Hero() {
             <p className="text-xl mb-8">
               Descubra o imóvel perfeito com a ReactImóveis
             </p>
-            <button className="bg-emerald-400 text-white px-4 py-2 rounded-md">
-              Ver imóveis
-            </button>
+            <input
+              type="text"
+              value={searchText}
+              onChange={(ev) => setSearchText(ev.target.value)}
+              placeholder="Pesquise um imóvel"
+              className="w-56 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-violet-400 focus:border-violet-400"
+            />
           </div>
           <div className="md:w-1/2">
             <img
--- src/providers/property-provider.jsx
+++ src/providers/property-provider.jsx
@@ -4,6 +4,28 @@ export const PropertiesContext = createContext();
 
 export function PropertiesProvider({ children }) {
   const [properties, setProperties] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (searchText === "") {
+      fetch("./src/data/properties.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setProperties(data);
+        });
+    }
+
+    setProperties(
+      properties.filter(
+        (property) =>
+          property.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          property.description
+            .toLowerCase()
+            .includes(searchText.toLowerCase()) ||
+          property.location.toLowerCase().includes(searchText.toLowerCase()),
+      ),
+    );
+  }, [searchText]);
 
   useEffect(() => {
     fetch("./src/data/properties.json")
@@ -14,7 +36,9 @@ export function PropertiesProvider({ children }) {
   }, []);
 
   return (
-    <PropertiesContext.Provider value={{ properties }}>
+    <PropertiesContext.Provider
+      value={{ properties, searchText, setSearchText }}
+    >
       {children}
     </PropertiesContext.Provider>
   );
